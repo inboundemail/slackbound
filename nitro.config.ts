336b9ee92c6092e850940c1473f52e3c31da203d
malewis5 <matthew.a.lewis7@gmail.com>
2025-08-06 16:45:48 -0400

    template

--- nitro.config.ts
+++ nitro.config.ts
@@ -3,6 +3,6 @@ import { defineNitroConfig } from 'nitropack/config';
 // https://nitro.build/config
 export default defineNitroConfig({
   compatibilityDate: 'latest',
-  srcDir: 'app/server',
+  srcDir: 'src/server',
   imports: false,
 });
